6c64e42d1736db18e8d170e4c2699b1526dcf375
Maye <codingmaye@outlook.com>
2023-08-28 15:06:13 +0200

    feat: Set ouput directory to CM_RESULT

--- cm-webp-encode.sh
+++ cm-webp-encode.sh
@@ -3,10 +3,15 @@
 # Compress all files in directory
 FILES="./*"
 alias cwebp="./bin/cwebp"
+FOLDER_OUTPUT="CM_RESULT"
+
+# Create result folder
+mkdir $FOLDER_OUTPUT
 
 for f in $FILES
 do
     echo "Converting $f to webp...."
     # convert each image file to webp
     cwebp "$f" -o "$f".webp
+    mv "$f".webp $FOLDER_OUTPUT
 done
